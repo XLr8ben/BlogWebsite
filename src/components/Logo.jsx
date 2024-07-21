9d90892c5bc1eda27999832faf117c59273bc5f8
Ribhav <113667610+XLr8ben@users.noreply.github.com>
2024-07-21 15:43:34 +0530

    Add files via upload

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className=' w-24'>
+      <img src='Amazon_logo.svg' alt='Logo'></img>
+    </div>
+  )
+}
+
+export default Logo
